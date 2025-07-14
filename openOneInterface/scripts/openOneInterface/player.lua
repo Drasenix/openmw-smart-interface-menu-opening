07d815aa0aef04e6fca9968436f6dcf798c27b60
Drasenix <adowns@hotmail.fr>
2025-07-14 19:38:06 +0200

    Basic test to open individual interface menu

--- openOneInterface/scripts/openOneInterface/player.lua
+++ openOneInterface/scripts/openOneInterface/player.lua
@@ -1,15 +1,35 @@
 local core = require('openmw.core')
-local UI = require('openmw.interfaces').UI
+local input = require('openmw.input')
+local I = require('openmw.interfaces')
 local configPlayer = require('scripts.clickToCloseMenu.config.player')
 local l10n = core.l10n('clickToCloseMenu')
+local ui = require('openmw.ui')
 
+local function onKeyPress(key)
+   
+   if key.code == input.KEY.I then
+      I.UI.setMode('Interface', {windows = {'Inventory'}})
+   end
 
-local function onMouseButtonPress(button)
-   print("button pressed")  
+   if key.code == input.KEY.M then
+      I.UI.setMode('Interface', {windows = {'Map'}})
+   end
+
+   if key.code == input.KEY.O then
+      I.UI.setMode('Interface', {windows = {'Magic'}})
+   end
+
+   if key.code == input.KEY.H then
+      I.UI.setMode('Interface', {windows = {'Stats'}})
+   end
+
+   
+   
+   
 end
 
 return {
    engineHandlers = {
-      onMouseButtonPress = onMouseButtonPress
+      onKeyPress = onKeyPress
    }
 }
--- openOneInterface/scripts/openOneInterface/settings/player.lua
+++ openOneInterface/scripts/openOneInterface/settings/player.lua
@@ -1,5 +1,6 @@
 local core = require('openmw.core')
 local I = require('openmw.interfaces')
+local input = require("openmw.input")
 
 local l10n = core.l10n('OpenOneInterface')
 local versionString = "1.0.0"
@@ -18,21 +19,38 @@ I.Settings.registerGroup {
     l10n = 'OpenOneInterface',
     name = 'ConfigCategoryClientOptions',
     permanentStorage = true,
-    settings = {                
+    settings = {        
         {
-            key = 's_Click',
-            renderer = 'select',
-            name = 'Click',
-            argument = {
-                l10n = 'OpenOneInterface',
-                items = {
-                    'Right',
-                    'Left',
-                    'Middle',
-                    'None'
-                }
-            },
-            default = 'Right',
-        }
+            key = 's_Key_Inventory',
+            renderer = 'inputKeySelection',
+            argument = {},
+            name = 'Key_Inventory',
+            description = 'Inventory',
+            default = input.KEY.I,
+        },
+        {
+            key = 's_Key_Map',
+            renderer = 'inputKeySelection',
+            argument = {},
+            name = 'Key_Map',
+            description = 'Map',
+            default = input.KEY.M,
+        },
+        {
+            key = 's_Key_Spells',
+            renderer = 'inputKeySelection',
+            argument = {},
+            name = 'Key_Spells',
+            description = 'Spells',
+            default = input.KEY.O,
+        },
+        {
+            key = 's_Key_Stats',
+            renderer = 'inputKeySelection',
+            argument = {},
+            name = 'Key_Stats',
+            description = 'Stats',
+            default = input.KEY.H,
+        },
     },
 }
