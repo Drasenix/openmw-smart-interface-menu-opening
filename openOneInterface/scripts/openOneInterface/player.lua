42c42ca9a931bd26b370a36bfbba625054d41543
Drasenix <adowns@hotmail.fr>
2025-07-15 04:27:31 +0200

    Using events instead of setting manually the mode

--- openOneInterface/scripts/openOneInterface/player.lua
+++ openOneInterface/scripts/openOneInterface/player.lua
@@ -1,5 +1,6 @@
 local core = require('openmw.core')
 local input = require('openmw.input')
+local self = require('openmw.self')
 local I = require('openmw.interfaces')
 local configPlayer = require('scripts.openOneInterface.config.player')
 local ui = require('openmw.ui')
@@ -9,66 +10,66 @@ local function onKeyPress(key)
    windows = {}
    if key.code == configPlayer.options_atoms.s_Key_Inventory then
       table.insert(windows, 'Inventory')
-      I.UI.setMode('Interface', {windows = windows})
+      self:sendEvent('AddUiMode', {mode = 'Interface', windows = windows})      
    end
 
    if key.code == configPlayer.options_atoms.s_Key_Map then
       table.insert(windows, 'Map')
-      I.UI.setMode('Interface', {windows = windows})
+      self:sendEvent('AddUiMode', {mode = 'Interface', windows = windows})      
    end
 
    if key.code == configPlayer.options_atoms.s_Key_Magic then
       table.insert(windows, 'Magic')
-      I.UI.setMode('Interface', {windows = windows})
+      self:sendEvent('AddUiMode', {mode = 'Interface', windows = windows})      
    end
 
    if key.code == configPlayer.options_atoms.s_Key_Stats then
       table.insert(windows, 'Stats')
-      I.UI.setMode('Interface', {windows = windows})
+      self:sendEvent('AddUiMode', {mode = 'Interface', windows = windows})      
    end
 
    if key.code == configPlayer.options_pairs.s_Key_Inventory_Map then
       table.insert(windows, 'Inventory')
       table.insert(windows, 'Map')
-      I.UI.setMode('Interface', {windows = windows})
+      self:sendEvent('AddUiMode', {mode = 'Interface', windows = windows})      
    end
    
    if key.code == configPlayer.options_pairs.s_Key_Inventory_Magic then
       table.insert(windows, 'Inventory')
       table.insert(windows, 'Magic')
-      I.UI.setMode('Interface', {windows = windows})
+      self:sendEvent('AddUiMode', {mode = 'Interface', windows = windows})      
    end
    
    if key.code == configPlayer.options_pairs.s_Key_Inventory_Stats then
       table.insert(windows, 'Inventory')
       table.insert(windows, 'Stats')
-      I.UI.setMode('Interface', {windows = windows})
+      self:sendEvent('AddUiMode', {mode = 'Interface', windows = windows})      
    end
    
    if key.code == configPlayer.options_pairs.s_Key_Map_Magic then
       table.insert(windows, 'Map')
       table.insert(windows, 'Magic')
-      I.UI.setMode('Interface', {windows = windows})
+      self:sendEvent('AddUiMode', {mode = 'Interface', windows = windows})      
    end
 
    if key.code == configPlayer.options_pairs.s_Key_Magic_Stats then
       table.insert(windows, 'Magic')
       table.insert(windows, 'Stats')
-      I.UI.setMode('Interface', {windows = windows})
+      self:sendEvent('AddUiMode', {mode = 'Interface', windows = windows})      
    end
 
    if key.code == configPlayer.options_trios.s_Key_Inventory_Map_Magic then
       table.insert(windows, 'Inventory')
       table.insert(windows, 'Map')
       table.insert(windows, 'Magic')
-      I.UI.setMode('Interface', {windows = windows})
+      self:sendEvent('AddUiMode', {mode = 'Interface', windows = windows})      
    end
 
    if key.code == configPlayer.options_trios.s_Key_Inventory_Map_Stats then
       table.insert(windows, 'Inventory')
       table.insert(windows, 'Map')
       table.insert(windows, 'Stats')
-      I.UI.setMode('Interface', {windows = windows})
+      self:sendEvent('AddUiMode', {mode = 'Interface', windows = windows})      
    end
 end
 
